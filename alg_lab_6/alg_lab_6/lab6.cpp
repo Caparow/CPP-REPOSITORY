f030fefcb1f0d07f7b125f1783a33f433af1a80d
Cypress <arowcap@gmail.com>
2016-12-30 14:51:55 +0300

    re-done...

--- alg_lab_6/alg_lab_6/lab6.cpp
+++ alg_lab_6/alg_lab_6/lab6.cpp
@@ -1,126 +1,124 @@
 #include <iostream>
 #include <math.h>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
+struct spline
+{
+	double a, b, c, d, x;
+};
 
-const int N = 30;
-const int A = 3;
+const int N = 100; //колличество кубических сплайнов на функцию
+const int A = 3; 
 const int B = 12;
 
-typedef double matrix[N + 1][N + 2];
-typedef double vect[N + 1];
+//интерполируемая функция
+double f(double x) { return 0.025*exp(x - 5)*sin(2.5*x); }
 
-double F(double x){	return 0.025*exp(x-5)*sin(2.5*x); }
-
-void GenerateSLAR(double(*f)(double), int n, double a, double b, matrix& res)
+//создание СЛАР для нахождения С коеффициентов
+vector<vector<double>> GenSystem(int n, double a, double b) 
 {
+	vector<vector<double>> res;
+	vector<double> tmp;
+	double x, xp, xn;
 	double h = (b - a) / n;
-	for (int i = 0; i <= n; i++)
-		for (int j = 0; j <= n + 1; j++)
-			res[i][j] = 0;
 
-	for (int i = 1; i < n; i++)
+	for (int i = 0; i <= n + 1; ++i)
+		tmp.push_back(0);
+	for (int i = 0; i <= n; ++i)
+		res.push_back(tmp);
+	tmp.clear();
+
+	for (int i = 1; i <= n; ++i)
 	{
-		if (i > 0) res[i][i - 1] = h;
-		res[i][i] = 4 * h;
-		if (i < n) res[i][i + 1] = h;
-		double xm1 = a + h * (i - 1);
-		double x1 = a + h * i;
-		double xp1 = a + h * (i + 1);
-		res[i][n + 1] = 6 * ((f(xp1) + f(xm1) - 2 * f(x1)) / h);
+		res[i][i] = 2 * (h + h);
+		res[i][i - 1] = h;
+		res[i][i + 1] = h;
+		x = a + h * i;
+		xp = a + h * (i - 1);
+		xn = a + h * (i + 1);
+		res[i][i+1] = 6 * ((f(xn) + f(xp) - 2 * f(x)) / h);
 	}
 	res[0][0] = 1;
 	res[0][n + 1] = 0;
 	res[n][n] = 1;
 	res[n][n + 1] = 0;
+
+	return res;
 }
 
-void CalcDB(double(*f)(double), const vect& vC, vect& vD, vect& vB, vect& vA, int n, double a, double b, vect& x)
+//решение СЛАР схемой единого деления (исключение Гаусса)(метод взят из 5ой лабораторной работы)
+vector<double> GaussEllimination(vector< vector<double> > A) {
+	int n = A.size();
+
+	for (int i = 0; i<n; i++) {
+		// Search for maximum in this column
+		double maxEl = abs(A[i][i]);
+		int maxRow = i;
+		for (int k = i + 1; k<n; k++) {
+			if (abs(A[k][i]) > maxEl) {
+				maxEl = abs(A[k][i]);
+				maxRow = k;
+			}
+		}
+
+		// Swap maximum row with current row (column by column)
+		for (int k = i; k<n + 1; k++) {
+			double tmp = A[maxRow][k];
+			A[maxRow][k] = A[i][k];
+			A[i][k] = tmp;
+		}
+
+		// Make all rows below this one 0 in current column
+		for (int k = i + 1; k<n; k++) {
+			double c = -A[k][i] / A[i][i];
+			for (int j = i; j<n + 1; j++) {
+				if (i == j) {
+					A[k][j] = 0;
+				}
+				else {
+					A[k][j] += c * A[i][j];
+				}
+			}
+		}
+	}
+
+	// Solve equation Ax=b for an upper triangular matrix A
+	vector<double> x(n);
+	for (int i = n - 1; i >= 0; i--) {
+		x[i] = A[i][n] / A[i][i];
+		for (int k = i - 1; k >= 0; k--) {
+			A[k][n] -= A[k][i] * x[i];
+		}
+	}
+	return x;
+}
+
+//расчет коеффициентов сплайнов
+vector<spline> GenSpline(vector<double> c, int n, double a, double b)
 {
-	x[0] = a;
-	vA[0] = f(a);
+	vector<spline> res;
 	double h = (b - a) / n;
-	for (int i = 1; i <= n; i++)
+	spline tmp;
+	res.push_back(tmp);
+
+	for (int i = 1; i <= N; ++i)
 	{
-		x[i] = a + (i * (b - a)) / n;
-		vA[i] = f(a + ((i*(b - a)) / n));
-		vD[i] = (vC[i] - vC[i - 1]) / h;
-		vB[i] = h*vC[i] / 2 - h*h*vD[i] / 6 + (f(a + h * i) - f(a + h * (i - 1))) / h;
-	}
-}
-
-void CopyMatrix(matrix& dest, const matrix src, int n)
-{
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j <= n; j++)
-			dest[i][j] = src[i][j];
-}
-
-bool GaussianElimination(const matrix SLAR, int n, vect& X)
-{
-	matrix a;
-	CopyMatrix(a, SLAR, n);
-
-	for (int k = 0; k < n; k++)
-	{
-		if (a[k, k] == 0)
-		{
-			cout << "Divide by zero!" << endl;
-			return false;
-		}
-
-		double e = a[k][k];
-		for (int j = k; j <= n; j++)
-			a[k][j] = a[k][j] / e;
-
-		for (int i = k + 1; i < n; i++)
-		{
-			e = a[i][k];
-			for (int j = 0; j <= n; j++)
-				a[i][j] = a[i][j] - a[k][j] * e;
-		}
+		spline tmp;
+		tmp.x = a + i*h;
+		tmp.c = c[i];
+		tmp.a = f(tmp.x);
+		tmp.d = (c[i] - c[i - 1]) / h;
+		tmp.b = h*c[i] / 2 - h*h*tmp.d / 6 + (f(tmp.x) - f(tmp.x - h)) / h;
+		res.push_back(tmp);
 	}
 
-	for (int i = n - 1; i >= 0; i--)
-	{
-		double sub = 0;
-		for (int j = i + 1; j < n; j++)
-			sub += a[i][j] * X[j];
-
-		X[i] = a[i][n] - sub;
-	}
-	return true;
+	return res;
 }
 
-
-void PrintVect(vect const X, int n)
-{
-	for (int i = 0; i < n; i++)
-		cout << "X[" << i << "]=" << X[i] << endl;
-	cout << endl;
-}
-
-void PrintParth(double c, double xi)
-{
-	if (c == 0) return;
-
-	if (c < 0)
-		cout << " - " << 0.0 + abs(c) << "*";
-	else
-		cout << " + " << c << "*";
-
-
-	if (xi == 0)
-		cout << "x";
-	else
-		if (xi < 0)
-			cout << "(x + " << 0.0 + abs(xi) << ")";
-		else
-			cout << "(x - " << xi << ")";
-}
-
+//расчет отдельного члена функции сплайна
 double CalcSt(double c, double xi, double x, int st) {
 	double tmp = 0;
 	if (xi == 0)
@@ -130,56 +128,54 @@ double CalcSt(double c, double xi, double x, int st) {
 			tmp = (x + 0.0 + abs(xi));
 		else
 			tmp = (x - xi);
-	
+
 	tmp = pow(tmp, st);
 
 	if (c < 0)
-		tmp *=-( 0.0 + abs(c));
+		tmp *= -(0.0 + abs(c));
 	else
 		tmp *= c;
 
 	return tmp;
 }
 
-void PrintSpline(vect const vA, vect const vB, vect const vC, vect const vD, int n, double a, double b)
+void CalcSplineF(vector<spline> sp, int n, double a, double b)
 {
-	for (int i = 1; i <= n; i++)
-	{
-		double xi = a + (i * (b - a)) / n;
-		cout << "s" << i << "(x)=" << vA[i];
-		PrintParth(vB[i], xi);
-		PrintParth(vC[i] / 2., xi);
-		cout << "^2 ";
-		PrintParth(vD[i] / 6., xi);
-		cout << "^3,  [" << a + ((i - 1) * (b - a)) / n << ", " << a + (i * (b - a)) / n << "]" << endl;
-	}
-
+	double h = (b - a) / n;
 	ofstream table("table.csv");
 	double tmp = 0;
-	for (int i = 1; i <= n; ++i) {
+
+	for (int i = 1; i < n; ++i)
+	{
 		double xi = a + (i * (b - a)) / n;
-		for (double x = a + ((i - 1) * (b - a)) / n; x <= a + (i * (b - a)) / n; x += 0.05) {
-			tmp += vA[i];
-			tmp += CalcSt(vB[i], xi, x, 1);
-			tmp += CalcSt(vC[i] / 2., xi, x, 2);
-			tmp += CalcSt(vD[i] / 6., xi, x, 3);
+		for (double x = a + (i - 1)*h + h / 5; x <= a + i*h + h / 5; x += h / 2) // '+h/5' для расчета не в узлах интерполяции
+		{
+			tmp += sp[i].a;
+			tmp += CalcSt(sp[i].b, xi, x, 1);
+			tmp += CalcSt(sp[i].c / 2., xi, x, 2);
+			tmp += CalcSt(sp[i].d / 6., xi, x, 3);
 			table << x << ";" << tmp << endl;
 			tmp = 0;
 		}
 	}
 	table.close();
-	cout << endl;
 }
 
 int main()
 {
-	matrix SLAR;
-	GenerateSLAR(F, N, A, B, SLAR);
+	cout << "Generating system..." << endl;
+	vector<vector<double>> mat = GenSystem(N, A, B);
 
-	vect c, d, b, a, x;
-	GaussianElimination(SLAR, N + 1, c);
-	CalcDB(F, c, d, b, a, N, A, B, x);
+	cout << "Finding C coeff..." << endl;
+	vector<double> c = GaussEllimination(mat); 
 
-	PrintSpline(a, b, c, d, N, A, B);
+	cout << "Generating spline functions..." << endl;
+	vector<spline> sp = GenSpline(c, N, A, B);
+
+	cout << "Calculating spline functions..." << endl;
+	CalcSplineF(sp, N, A, B);
+
+	cout << "Interpolation function is ready in the 'table.csv' file." << endl << endl;
+
 	return 0;
 }
